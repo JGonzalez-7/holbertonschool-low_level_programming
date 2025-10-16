15d5bb9982c5d2173f69c945dc9cf9fd376a43b9
Joshua <joshgonzalez027@gmail.com>
2025-10-16 00:01:06 -0400

    Adding task 4 from C - Variables, if, else, while

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main - prints the alphabet in lowercase except q and e
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+if (alphabet != 'e' && alphabet != 'q')
+
+putchar(alphabet);
+
+putchar('\n');
+
+return (0);
+}
