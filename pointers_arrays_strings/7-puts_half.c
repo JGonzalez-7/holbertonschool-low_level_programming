cbe4cfa5cc45d42e392c8d117329503e6811cbca
Joshua <joshgonzalez027@gmail.com>
2025-10-24 15:41:40 -0400

    Adding a function that prints half of a string, with main.c file

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,27 +11,27 @@
  */
 void puts_half(char *str)
 {
-    int length = 0; /* Variable to count the string length */
-    int start;      /* Variable for where to start printing */
+	int length = 0; /* Variable to count the string length */
+	int start;      /* Variable for where to start printing */
 
-    /* Find the length of the string */
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+	/* Find the length of the string */
+	while (str[length] != '\0')
+	{
+		length++;
+	}
 
-    /* Calculate where to start printing */
-    if (length % 2 == 0)
-        start = length / 2;         /* even length */
-    else
-        start = (length + 1) / 2;   /* odd length */
+	/* Calculate where to start printing */
+	if (length % 2 == 0)
+		start = length / 2;         /* even length */
+	else
+		start = (length + 1) / 2;   /* odd length */
 
-    /* Print from the middle to the end */
-    while (str[start] != '\0')
-    {
-        _putchar(str[start]);
-        start++;
-    }
+	/* Print from the middle to the end */
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
