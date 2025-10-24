bb371b9fd02247616d68e253c0db1fbeca98274f
Joshua <joshgonzalez027@gmail.com>
2025-10-24 14:16:24 -0400

    Adding a function that prints every other character of a string, starting with the first character; with main.c file

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to the string to print
+ *
+ * Description: Starts at index 0 and prints every second character
+ * (index 0, 2, 4, etc.), then prints a newline at the end.
+ * Return: void (no return value)
+ */
+void puts2(char *str)
+{
+    int i = 0; /* Counter for string index */
+
+    /* Loop through string until null terminator */
+    while (str[i] != '\0')
+    {
+        /* Print only characters with even index */
+        if (i % 2 == 0)
+        {
+            _putchar(str[i]);
+        }
+        i++;
+    }
+
+    _putchar('\n');
+}
