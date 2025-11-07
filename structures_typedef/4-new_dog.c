3d3c75c96679314a1dd8ad76ada368215e7449cf
Joshua <joshgonzalez027@gmail.com>
2025-11-06 22:13:30 -0400

    Adding a fix to the function

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,10 +1,8 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-
 /**
- * new_dog - creates a new dog and prints a message
+ * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -13,24 +11,14 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d = malloc(sizeof(dog_t)); /* allocate memory for dog */
-		if (!d)
+	dog_t *d = malloc(sizeof(dog_t)); /* allocate memory for struct */
+
+	if (!d)
 		return (NULL);
 
-	d->name = name;    /* just assign pointers, no copying */
+	d->name = name;  /* assign pointer */
 	d->age = age;
 	d->owner = owner;
 
-	/* Print the custom message */
-	printf("My name is %s, and I am %.1f :) - Woof!\n", d->name, d->age);
-
 	return (d);
-}
-
-int main(void)
-{
-	dog_t *p = new_dog("Poppy", 3.5, "Alice");
-
-	free(p); /* free memory */
-	return (0);
 }
