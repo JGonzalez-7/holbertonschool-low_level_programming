a88a61620a840447a8f6c4da51de57ecf96d9829
Joshua <joshgonzalez027@gmail.com>
2025-11-07 15:16:30 -0400

    Added comments to each lines of the functions for better undertanding

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,33 @@
-#include <stdio.h>
+#include <stdio.h>  /* Include standard input/output library */
 
 /**
- * main - prints numbers 1 to 100 with FizzBuzz rules
+ * main - prints numbers from 1 to 100 following FizzBuzz rules
  *
- * Description: Prints numbers from 1 to 100.
- * For multiples of 3, prints "Fizz".
- * For multiples of 5, prints "Buzz".
- * For multiples of both 3 and 5, prints "FizzBuzz".
+ * Description: For multiples of 3, prints "Fizz"; for multiples of 5, prints
+ * "Buzz"; for numbers divisible by both 3 and 5, prints "FizzBuzz". Each
+ * number or word is separated by a space and ends with a new line.
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;
+	int num;  /* Variable to store current number */
 
-	for (num = 1; num <= 100; num++)
+	for (num = 1; num <= 100; num++)  /* Loop from 1 to 100 */
 	{
-		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
-		else if (num % 3 == 0)
-			printf("Fizz");
-		else if (num % 5 == 0)
-			printf("Buzz");
+		if (num % 3 == 0 && num % 5 == 0)  /* If divisible by both 3 and 5 */
+			printf("FizzBuzz");  /* Print FizzBuzz */
+		else if (num % 3 == 0)  /* If divisible by 3 only */
+			printf("Fizz");  /* Print Fizz */
+		else if (num % 5 == 0)  /* If divisible by 5 only */
+			printf("Buzz");  /* Print Buzz */
 		else
-			printf("%d", num);
+			printf("%d", num);  /* Otherwise, print the number */
 
-		if (num < 100)
+		if (num < 100)  /* Add a space after each output except the last one */
 			printf(" ");
 	}
 
-	printf("\n");
-	return (0);
+	printf("\n");  /* Print newline at the end */
+	return (0);  /* Indicate successful program execution */
 }
